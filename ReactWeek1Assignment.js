d108c18a0285e0ca9d13390a3f0e2397fbcac5f0
Marina Paskal <paskalm@rogers.com>
2021-10-02 12:32:23 -0400

    Complete registration of the students

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -15,16 +15,27 @@ class Bootcamp {
 
   registerStudent(student) {
     const studentReg = this.students.filter((st) => st.email === student.email);
-    if (studentReg.length > 0) {
+    if (studentReg.length) {
       console.log(
-        `Student  ${student.email} already registered to the bootcamp "Web Dev Fundamentals".`
+        `Student  ${student.email} already registered to the bootcamp ${this.name}.`
       );
     } else {
       this.students.push(student);
       console.log(
-        `Student ${student.email} registred to the bootcamp "Web Dev Fundamentals".`
+        `Student ${student.email} registred to the bootcamp ${this.name}.`
       );
     }
     return this.students;
   }
 }
+
+const st1 = new Student("Mark", "mark@gmail.com", "React");
+const st2 = new Student("Christina", "christina@gmail.com", "Java");
+const st3 = new Student("Dana", "dana@gmail.com", "JS");
+
+const bootcamp1 = new Bootcamp("React", "1");
+
+bootcamp1.registerStudent(st1);
+bootcamp1.registerStudent(st2);
+bootcamp1.registerStudent(st3);
+bootcamp1.registerStudent(st1);
