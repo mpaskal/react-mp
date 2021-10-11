f6e085e7c4ca1ea45d34ec2121735042ae10b824
Marina Paskal <paskalm@rogers.com>
2021-10-11 18:06:15 -0400

    Add functional components

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,36 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function RenderDirectoryItem({ campsite, onClick }) {
+  return (
+    <Card onClick={() => onClick(campsite.id)}>
+      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+      <CardImgOverlay>
+        <CardTitle>{campsite.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+}
 
-  onCampsiteSelect(campsite) {
-    this.setState({ selectedCampsite: campsite });
-  }
-
-  render() {
-    const directory = this.props.campsites.map((campsite) => {
-      return (
-        <div key={campsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(campsite.id)}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-              <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
-
+function Directory(props) {
+  const directory = props.campsites.map((campsite) => {
     return (
-      <div className="container">
-        <div className="row">{directory}</div>
+      <div key={campsite.id} className="col-md-5 m-1">
+        <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="container">
+      <div className="row">{directory}</div>
+    </div>
+  );
 }
 
 export default Directory;
